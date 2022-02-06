032277b958fa51edc0bbfebf016dcd8667af6956
Yuliia Khrupina <julichka1391@gmail.com>
2022-02-05 18:20:34 -0800

    added Pan Gesture Recognizer and UI for the app

--- MiniGame/ViewController.swift
+++ MiniGame/ViewController.swift
@@ -9,9 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var view1: UIView!
+    @IBOutlet weak var view2: UIView!
+    @IBOutlet weak var view3: UIView!
+    @IBOutlet weak var view4: UIView!
+    @IBOutlet weak var view5: UIView!
+    
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    @IBAction func onPanGesture(_ sender: UIPanGestureRecognizer) {
+        
     }
 }
 
